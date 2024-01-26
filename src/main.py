d4bebb60899a88e0adfb2cae5492c24bb68b7758
tofa <tom.fankhaenel@t-systems.com>
2024-01-26 13:49:53 +0100

    tested and working

--- src/main.py
+++ src/main.py
@@ -6,10 +6,15 @@ import time
 # Set your AWS credentials (or use other methods to configure credentials)
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-region_name = os.environ.get('AWS_REGION')
+region_name = os.environ.get('AWS_REGION', 'eu-central-1')
 zone_id = os.environ.get('ROUTE53_ZONE')
 # todo allow multiple records
-record_name = os.environ.get('*.e-suffer.de')
+records = os.environ.get('ROUTE53_RECORDS')
+# split env by ","
+records = records.split(',')
+
+def convert_to_record_set_format(name):
+    return name.replace('*', '\\052')
 
 def get_current_ip():
     # Use a service like 'httpbin' to get the public IP address
@@ -22,17 +27,16 @@ def get_route53_ip(zone_id, record_name):
     if not (aws_access_key_id and aws_secret_access_key and region_name):
         print("AWS credentials or region not provided in the environment variables.")
         return None
-
     # Initialize Route 53 client
     route53_client = boto3.client('route53', aws_access_key_id=aws_access_key_id,
                                   aws_secret_access_key=aws_secret_access_key, region_name=region_name)
-
     try:
         # Get the hosted zone details
-        response = route53_client.list_resource_record_sets(HostedZoneId=zone_id)
-        
+        response = route53_client.list_resource_record_sets(HostedZoneId=zone_id, StartRecordName=record_name)
         # Search for the specific record in the response
         for record_set in response['ResourceRecordSets']:
+            # convert the record_name to match backslash escaped format of records
+            record_name = convert_to_record_set_format(record_name)
             if record_set['Name'] == record_name and record_set['Type'] == 'A':
                 # Assuming A record type, you can modify for other types
                 return record_set['ResourceRecords'][0]['Value']
@@ -44,10 +48,6 @@ def get_route53_ip(zone_id, record_name):
         return None
 
 def update_route53_record(zone_id, record_name, new_ip):
-    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    region_name = os.environ.get('AWS_DEFAULT_REGION')
-
     if not (aws_access_key_id and aws_secret_access_key and region_name):
         print("AWS credentials or region not provided in the environment variables.")
         return False
@@ -78,7 +78,7 @@ def update_route53_record(zone_id, record_name, new_ip):
                 ],
             }
         )
-        print(f"Route 53 record updated successfully. New IP: {new_ip}")
+        print(f"Route 53 record " + record_name + " updated to " + new_ip)
         return True
 
     except Exception as e:
@@ -87,9 +87,13 @@ def update_route53_record(zone_id, record_name, new_ip):
 
 while True:
     current_ip = get_current_ip()
-    route53_ip = get_route53_ip(zone_id, record_name)
+    for record_name in records:
+        route53_ip = get_route53_ip(zone_id, record_name)
 
-    if current_ip and route53_ip and current_ip != route53_ip:
-        update_route53_record(zone_id, record_name, current_ip)
-
+        if current_ip != route53_ip:
+            print("current: " + current_ip + " old: " + route53_ip + " for record: " + record_name + " updating to current ip ...")
+            update_route53_record(zone_id, record_name, current_ip)
+        else:
+            print("current: " + current_ip + " old: " + route53_ip + " for record: " + record_name + " nothing to do ...")
+    print("sleeping a while")
     time.sleep(300)  # Sleep for 5 minutes before checking again
